84b6f5b417920de46eed99df1942a76e66a0e72d
Vincent Ambo <v.ambo@me.com>
2012-04-12 02:56:33 +0200

    * colouring tool changes

--- tools/colouriser/colour.hs
+++ tools/colouriser/colour.hs
@@ -9,13 +9,16 @@ defineOptions "MainOptions" $ do
 	stringOption "optFile" "file" ""
 		"Name of the .hs file. Will be used for the HTML file as well"
 
-colorCode :: String -> String -> IO ()
-colorCode input output = do
+colorCode :: String -> IO ()
+colorCode input = do
 	code <- readFile input
-	writeFile output $ hscolour False code
+	putStr $ concat [ "<div class=\"code\">"
+		     		, hscolour False code
+				    , "</div>"
+				    ]
 
 main :: IO ()
 main = runCommand $ \opts args -> do
 	let file = optFile opts
 	unless (file == "") $
-		colorCode (file ++ ".hs") (file ++ ".html")
+		colorCode file
