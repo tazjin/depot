ee536eb9b79f5dd7d5c0c7882de0759740affba3
Vincent Ambo <vincent@spotify.com>
2013-07-01 19:09:26 +0200

    Emacs: Added IRC config

--- init.el
+++ init.el
@@ -39,3 +39,24 @@
 ;; Add keybindings to move nested blocks with C-, rsp. C-.
 (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
 (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)
+
+;; IRC configuration (erc)
+;; Actual servers and such are loaded from irc.el
+(require 'erc)
+(load "~/.emacs.d/irc")
+
+;; Hiding JOIN, QUIT, PART
+(setq erc-hide-list '("JOIN" "PART" "QUIT"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(erc-modules (quote (autojoin button completion dcc irccontrols list log match menu move-to-prompt netsplit networks noncommands notifications readonly ring scrolltobottom stamp track))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
