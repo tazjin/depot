a5a7db8f1b541a78613fe0696887151e3564b0e3
Vincent Ambo <vincent@spotify.com>
2013-07-04 22:49:26 +0200

    emacs: Added clojure stuff

--- init.el
+++ init.el
@@ -15,7 +15,8 @@
                   magit
                   color-theme-solarized
                   projectile
-                  rainbow-delimiters)
+                  rainbow-delimiters
+                  nrepl)
   "A list of packages to install at launch.")
 
 (dolist (p my-pkgs)
@@ -86,6 +87,20 @@
 (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
 (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)
 
+;; Configure nreply (Clojure mode)
+(require 'nrepl)
+;; eldoc in clojure
+(add-hook 'nrepl-interaction-mode-hook
+  'nrepl-turn-on-eldoc-mode)
+
+;; Don't annoy me
+(setq nrepl-hide-special-buffers t)
+(setq nrepl-popup-stacktraces nil)
+
+;; Paredit in nrepl
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
+
 ;; IRC configuration (erc)
 ;; Actual servers and such are loaded from irc.el
 (require 'erc)
@@ -121,6 +136,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default)))
  '(erc-modules (quote (autojoin button completion dcc irccontrols list log match menu move-to-prompt netsplit networks noncommands notifications readonly ring scrolltobottom stamp track))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
