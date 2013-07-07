c8dc574fcd374c1e1cdc59e5da0331e5dc361106
Vincent Ambo <vincent@spotify.com>
2013-07-07 22:03:29 +0200

    emacs: - Move to Emacs.app (which doesn't work with Xnomad! :( But it's goood) - Add clojure-mode - Switch to leuven theme (again, thanks @pcarrier)

--- init.el
+++ init.el
@@ -1,7 +1,13 @@
 ;; Configure package manager
 (require 'package)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+;; Add Marmalade repo
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;; ... and melpa. Melpa packages that exist on marmalade will have
+;; precendence.
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -13,10 +19,11 @@
                   haskell-mode
                   markdown-mode
                   magit
-                  color-theme-solarized
+                  leuven-theme
                   projectile
                   rainbow-delimiters
                   nrepl
+                  clojure-mode
                   ace-jump-mode)
   "A list of packages to install at launch.")
 
@@ -46,7 +53,7 @@
 (el-get 'sync my:el-get-packages)
 
 ;; Set solarized theme
-(load-theme 'solarized-dark t)
+(load-theme 'leuven t)
 
 ;; Other general settings
 
@@ -70,6 +77,17 @@
 
 ;; Use clipboard properly
 (setq x-select-enable-clipboard t)
+
+;; Settings for Emacs.app (Cocoa Emacs)
+;; Menu bar doesn't take up additional space, so lets use it.
+(menu-bar-mode 1)
+
+;; Don't use Apple's native fullscreen (FIXME: Change with Mavericks)
+(setq ns-use-native-fullscreen nil)
+
+;; ... and then enable fullscreen. (This requires a nightly build of
+;; Emacs for OS X)
+;;(toggle-frame-fullscreen)
 
 ;; Navigate windows with M-<arrows>
 (windmove-default-keybindings 'meta)
@@ -111,8 +129,11 @@
 (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
 (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)
 
-;; Configure nreply (Clojure mode)
-(require 'nrepl)
+;; Configure nrepl (Clojure REPL) and clojure-mode
+;; Paredit in clojure
+
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
 ;; eldoc in clojure
 (add-hook 'nrepl-interaction-mode-hook
   'nrepl-turn-on-eldoc-mode)
@@ -160,8 +181,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default)))
- '(erc-modules (quote (autojoin button completion dcc irccontrols list log match menu move-to-prompt netsplit networks noncommands notifications readonly ring scrolltobottom stamp track))))
+ '(custom-safe-themes
+   (quote
+    ("ea0c5df0f067d2e3c0f048c1f8795af7b873f5014837feb0a7c8317f34417b04" "a234f91f9be6ed40f6ce0e94dce5cea1b9f1ccec2b9ccd42bb71c499867a3fcc" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default)))
+ '(erc-modules
+   (quote
+    (autojoin button completion dcc irccontrols list log match menu move-to-prompt netsplit networks noncommands notifications readonly ring scrolltobottom stamp track)))
+ '(ns-alternate-modifier (quote none))
+ '(ns-command-modifier (quote meta)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
