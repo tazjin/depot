fc76db3d96626ba449a37d603e2a44c3c7d78123
Vincent Ambo <tazjin@gmail.com>
2018-03-04 00:14:13 +0100

    refactor(init): Remove package installation configuration
    Package installation has moved to my NixOS configuration.

--- init.el
+++ init.el
@@ -1,86 +1,12 @@
 ;;; init.el --- Package bootstrapping. -*- lexical-binding: t; -*-
 
-;; This file bootstraps the Emacs setup by going through package installations.
-;; After all packages are installed, local configuration is loaded.
+;; Packages are installed via Nix configuration, this file only
+;; initialises the newly loaded packages.
 
 (require 'package)
 (require 'seq)
 
-;; Configure Marmalade and MELPA repositories. Packages available on Marmalade
-;; will have precedence.
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
-
-;; This variable controls all packages that should be installed.
-(defvar desired-packages
-  '(;; elisp libraries
-    dash
-    dash-functional
-    ht
-    s
-
-    ;; editor packages
-    ace-jump-mode
-    ace-window
-    adjust-parens
-    browse-kill-ring
-    cargo
-    company
-    dockerfile-mode
-    erlang
-    exwm
-    flycheck
-    geiser
-    go-mode
-    gruber-darker-theme
-    haskell-mode
-    helm
-    helm-exwm
-    helm-pass
-    hi2
-    idle-highlight-mode
-    magit
-    markdown-mode+
-    multi-term
-    multiple-cursors
-    nix-mode
-    paredit
-    password-store
-    pg
-    puppet-mode
-    racer
-    racket-mode
-    rainbow-delimiters
-    rainbow-mode
-    restclient
-    rust-mode
-    smart-mode-line
-    string-edit
-    sly
-    sly-company
-    sly-quicklisp
-    switch-window
-    terraform-mode
-    undo-tree
-    uuidgen
-    yaml-mode
-    which-key
-    ))
-
-(defun installable-packages (pkg-list)
-  "Filter out not-yet installed packages from package list."
-  (seq-filter (lambda (p) (not (package-installed-p p))) pkg-list))
-
-(defun install-needed-packages (pkg-list)
-  (let ((to-install (installable-packages pkg-list)))
-    (if (< 0 (length to-install))
-        (progn (package-refresh-contents)
-               (mapcar #'package-install to-install))
-      (message "No new packages to install."))))
-
-;; Run package installation!
-(install-needed-packages desired-packages)
 
 ;; Configure a few basics before moving on to package-specific initialisation.
 (setq custom-file (concat user-emacs-directory "init/custom.el"))
