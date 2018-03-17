eaac33465d7885ec71f161ad769f04103ff2f58f
Vincent Ambo <tazjin@gmail.com>
2018-03-17 22:06:52 +0100

    fix(custom): Mark gruber-darker safe, again.
    This comes up again and again and again and again, for reasons I don't
    understand or have time to care about.

--- init/custom.el
+++ init/custom.el
@@ -10,7 +10,7 @@
  '(custom-enabled-themes (quote (gruber-darker)))
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "89336ca71dae5068c165d932418a368a394848c3b8881b2f96807405d8c6b5b6" default)))
+    ("d61fc0e6409f0c2a22e97162d7d151dee9e192a90fa623f8d6a071dbf49229c6" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "89336ca71dae5068c165d932418a368a394848c3b8881b2f96807405d8c6b5b6" default)))
  '(elnode-send-file-program "/run/current-system/sw/bin/cat")
  '(frame-brackground-mode (quote dark))
  '(global-auto-complete-mode t)
