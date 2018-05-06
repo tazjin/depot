301a9c538df455111ac47c1c75eb29c4912a6093
Vincent Ambo <tazjin@gmail.com>
2018-05-06 21:44:48 +0200

    feat(bindings): Bind counsel-rg to 'C-c r g'

--- init/bindings.el
+++ init/bindings.el
@@ -5,12 +5,14 @@
 (define-key global-map (kbd "C--") 'text-scale-decrease)
 
 ;; Use regex searches by default.
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "\C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "M-%") 'query-replace-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 (global-set-key (kbd "C-M-%") 'query-replace)
+
+;; Counsel stuff:
+(global-set-key (kbd "C-c r g") 'counsel-rg)
 
 ;; Avy mode
 (global-set-key (kbd "M-j") 'avy-goto-char)
