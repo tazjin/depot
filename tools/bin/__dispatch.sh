27c3e77d2f7956340cf97bd575e3ac110848a05f
Vincent Ambo <tazjin@google.com>
2019-07-04 15:57:12 +0100

    feat(tools): Add custom GHC with dependencies to env

--- tools/bin/__dispatch.sh
+++ tools/bin/__dispatch.sh
@@ -19,6 +19,9 @@ case "${TARGET_TOOL}" in
   stylish-haskell)
     attr="haskellPackages.stylish-haskell"
     ;;
+  ghc)
+    attr="thirdParty.ghc"
+    ;;
   *)
     echo "The tool '${TARGET_TOOL}' is currently not installed in this repository."
     exit 1
