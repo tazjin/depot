bf2efeba2d5772ba1853ec5875c3c2faf02f1b07
Vincent Ambo <tazjin@google.com>
2019-08-21 11:07:09 +0100

    chore(tazblog): Introduce dependency on 'dns'

--- services/tazblog/tazblog.nix
+++ services/tazblog/tazblog.nix
@@ -1,5 +1,5 @@
 { mkDerivation, acid-state, base, base64-bytestring, blaze-html
-, blaze-markup, bytestring, cache, crypto-api, cryptohash, hamlet
+, blaze-markup, bytestring, cache, crypto-api, cryptohash, dns, hamlet
 , happstack-server, markdown, mtl, network, network-uri
 , old-locale, options, rss, shakespeare, stdenv, text
 , time, transformers
@@ -12,7 +12,7 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [
     base base64-bytestring blaze-html blaze-markup bytestring
-    cache crypto-api cryptohash hamlet happstack-server markdown mtl
+    cache crypto-api cryptohash dns hamlet happstack-server markdown mtl
     network network-uri old-locale rss shakespeare text time
     transformers
   ];
