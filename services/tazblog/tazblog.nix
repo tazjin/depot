593e96da6077b343b9716d490a17c7c3603cb2fc
Vincent Ambo <tazjin@google.com>
2019-08-25 23:21:26 +0100

    chore(tazblog): Clean up unused dependencies

--- services/tazblog/tazblog.nix
+++ services/tazblog/tazblog.nix
@@ -1,9 +1,6 @@
 { mkDerivation, aeson, acid-state, base, base64-bytestring, blaze-html
-, blaze-markup, bytestring, cache, crypto-api, cryptohash, dns, hamlet
-, happstack-server, markdown, mtl, network, network-uri
-, old-locale, options, rss, shakespeare, stdenv, text
-, time, transformers
-}:
+, bytestring, dns, happstack-server, markdown, network, network-uri, old-locale
+, options, rss, shakespeare, stdenv, text, time }:
 mkDerivation {
   pname = "tazblog";
   version = "6.0.0";
@@ -11,10 +8,21 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson base base64-bytestring blaze-html blaze-markup bytestring
-    cache crypto-api cryptohash dns hamlet happstack-server markdown mtl
-    network network-uri old-locale rss shakespeare text time
-    transformers
+    aeson
+    base
+    base64-bytestring
+    blaze-html
+    bytestring
+    dns
+    happstack-server
+    markdown
+    network
+    network-uri
+    old-locale
+    rss
+    shakespeare
+    text
+    time
   ];
   executableHaskellDepends = [ base network options ];
   description = "Tazjin's Blog";
