d9a0f07c73f3fa17b4f369a6c187468ee26b455a
Vincent Ambo <tazjin@google.com>
2019-09-03 00:25:40 +0100

    chore(third_party): Bump Nixery version

--- third_party/nixery.nix
+++ third_party/nixery.nix
@@ -5,8 +5,8 @@
 let src = pkgs.fetchFromGitHub {
   owner = "google";
   repo = "nixery";
-  rev = "5d64be1969f397236883f140b4d533497622387c";
-  sha256 = "0vxng5iy6m5a9b38x1mama6idagdinlv001fdkmjgj25sgyrqy2h";
+  rev = "811a1fd228906c89af61d340be8f44788152dad9";
+  sha256 = "0jsps86kr64rbnyi49w4pv9pr2fz1wp80zzk5a8ap6q21daky4q0";
 };
 in import src {
   inherit pkgs;
@@ -14,5 +14,5 @@ in import src {
     export USER=root
     cachix use tazjin
   '';
-  extraPackages = with pkgs; [ cachix iana-etc ];
+  extraPackages = with pkgs; [ cachix ];
 }
