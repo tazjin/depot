071babf14824aee035dd6c3ddff1957662aa7c98
Vincent Ambo <tazjin@google.com>
2019-11-24 20:34:30 +0000

    feat(example): Add an example for how to use buildGo builders

---
+++ example/main.go
@@ -0,0 +1,22 @@
+// Copyright 2019 Google LLC.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Package main provides a tiny example program for the Bazel-style
+// Nix build system for Go.
+
+package main
+
+import (
+	"example"
+	"exampleproto"
+	"fmt"
+)
+
+func main() {
+	thing := exampleproto.Thing{
+		Id:          example.UUID(),
+		KindOfThing: "test thing",
+	}
+
+	fmt.Printf("The thing is a %s with ID %q\n", thing.Id, thing.KindOfThing)
+}
