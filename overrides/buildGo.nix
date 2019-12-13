97e25e4dab96c77f499f360d6367e8d14a7cbbb3
Vincent Ambo <tazjin@google.com>
2019-12-13 21:31:37 +0000

    chore(overrides): Bump buildGo.nix to add ASM support
    This is required to build `x/sys`, which is required by the blog_cli

--- overrides/buildGo.nix
+++ overrides/buildGo.nix
@@ -2,5 +2,5 @@
 
 (import "${builtins.fetchGit {
   url = "https://github.com/tazjin/buildGo.nix";
-  rev = "5649c75d01dc9058d9a6092ad2a504669cc780a9";
+  rev = "7f74980457df843aea542510a406f34366e8b868";
 }}/buildGo.nix") { pkgs = pkgs.third_party; }
