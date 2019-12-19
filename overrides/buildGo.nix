11865364acaa4b03124c6beb73f6b7a18776eae6
Vincent Ambo <tazjin@google.com>
2019-12-19 15:24:00 +0000

    chore(overrides): Remove buildGo.nix to prepare for depot merge

--- overrides/buildGo.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-
-(import "${builtins.fetchGit {
-  url = "https://github.com/tazjin/buildGo.nix";
-  rev = "53630e3b1544de38f509e48ac0f589bcfafa7728";
-}}/buildGo.nix") { pkgs = pkgs.third_party; }
